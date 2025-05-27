76a8d4e80afd1dad239802aed786e0a4cdb29e36
abdulrahman305 <owner@sauditech.link>
2025-05-27 09:44:54 +0000

    feat: add JWT creation functionality with PEM file and client ID input

--- main.py
+++ main.py
@@ -1,7 +1,41 @@
 # Standard imports
+import sys
+import time
+import jwt
 import json
 import urllib.parse
 from typing import Any
+
+# Get PEM file path
+if len(sys.argv) > 1:
+    pem = sys.argv[1]
+else:
+    pem = input("Enter path of private PEM file: ")
+
+# Get the Client ID
+if len(sys.argv) > 2:
+    client_id = sys.argv[2]
+else:
+    client_id = input("Enter your Client ID: ")
+
+# Open PEM
+with open(pem, 'rb') as pem_file:
+    signing_key = pem_file.read()
+
+payload = {
+    # Issued at time
+    'iat': int(time.time()),
+    # JWT expiration time (10 minutes maximum)
+    'exp': int(time.time()) + 600,
+    
+    # GitHub App's client ID
+    'iss': client_id
+}
+
+# Create JWT
+encoded_jwt = jwt.encode(payload, signing_key, algorithm='RS256')
+
+print(f"JWT: {encoded_jwt}")
 
 # Third-party imports
 import sentry_sdk
